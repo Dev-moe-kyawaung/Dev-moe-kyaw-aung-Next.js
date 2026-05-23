3e2852d3a815b76aa75ba214ff02e39678f65848
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-23 19:50:01 +0630

    Create Preloader.jsx

---
+++ src/components/layout/Preloader.jsx
@@ -0,0 +1,8 @@
+// src/components/layout/Preloader.jsx
+export default function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="preloader-spinner" />
+    </div>
+  );
+}
